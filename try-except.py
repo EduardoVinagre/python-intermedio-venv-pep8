c2f5a2589b8b0ab2dd063892aefb40b4f475cd39
eduardo vinagre <eduardovinagre@gmail.com>
2026-04-07 06:32:00 -0600

    Se agrega manejor de excepción por interrupción del ususario con ctrl+c

--- try-except.py
+++ try-except.py
@@ -9,6 +9,8 @@ except ValueError:
     print("Error: Invalid input. Please enter a valid integer.")
 except ZeroDivisionError:
     print("Error: Division by zero is not allowed.")
+except KeyboardInterrupt:
+    print("\nError: Input interrupted by the user.")
 finally:
     print("This block will always execute, regardless of exceptions.")
 
